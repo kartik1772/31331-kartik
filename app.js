3fde85213b6c3726cc98a561461cb8118f136387
Kartik <kusmudekartik5418@gmail.com>
2022-08-08 12:08:52 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.send("welcome to  vcvcv the api"));
+
+app.post("/solve", (req, res) => {
+  try {
+    let arr = req.body.array;
+
+    if (!arr) {
+      return res.json({
+        status: false,
+        message: "array not found in body",
+      });
+    }
+
+    arr = JSON.parse(arr);
+
+    let numbers = [];
+    let alphabets = [];
+
+    arr.forEach((element) => {
+      if (parseInt(element) == element) numbers.push(element);
+      if (element.length === 1 && element.match(/[a-z]/i))
+        alphabets.push(element);
+    });
+
+    res.json({
+      status: true,
+      name: "Vedant Daigavane",
+      count: numbers.length + alphabets.length,
+      email:"kusmduekartik5418@gmail.coms",
+      numbers,
+      alphabets,
+    });
+  } catch (err) {
+    res.json({
+      status: false,
+      message: "error occured"+err,
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is up and runign on port " + 3000);
+});
